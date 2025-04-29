dee99d3cd9295b0a465696702a056f6fdb887425
urosaar <oussra061@gmail.com>
2025-04-29 17:17:39 +0100

    adding parsing

--- minishell.h
+++ minishell.h
@@ -5,5 +5,16 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+typedef struct s_command
+{
+    char *cmd;              // command name
+    char **args;            // arguments (including cmd)
+    char *infile;           // input redirection: < or <<
+    char *outfile;          // output redirection: > or >>
+    int append;             // 1 if >>
+    int heredoc;            // 1 if <<
+    struct s_command *next; // next command in pipeline
+} t_command;
+
 
 #endif
--- parssing.c
+++ parssing.c
@@ -1,5 +1,4 @@
 #include "minishell.h"
-
 /*i tryed to work with lixing because it will make things easy 
 It splits a raw string input into pieces like commands, arguments, pipes, redirects, etc 
 it is like a improved version of ft_split */
@@ -75,22 +74,24 @@ char *extract_word(const char *input, int *i)
 }
 
 /* Counts how many tokens exist in the input string */
-int count_tokens(const char *input)  // this is used for count the number of tokens in an input string
+int count_tokens(const char *input)
 {
-	int count = 0, i = 0;
+	int count = 0;
+    int i = 0;
 	while (input[i])
     {
 		while (is_space(input[i])) i++;
 		if (!input[i])
             break;
-		if (input[i] == '\'' || input[i] == '"')
-        {
-			i++;
-			while (input[i] && input[i] != input[i - 1])
-                i++;
-			if (input[i])
-                i++;
-		}
+        if (input[i] == '\'' || input[i] == '"')
+    {
+        char quote = input[i++];
+        while (input[i] && input[i] != quote)
+            i++;
+        if (input[i])
+            i++;
+    }
+
         else if (is_operator(input[i]))
         {
 			i++;
@@ -236,48 +237,205 @@ char *get_input(void)
     return input;
 }
 
+char    *ft_strdup(char *src)
+{
+    char    *dest;
+    int        i;
+
+    i = 0;
+    while (src[i])
+        i++;
+    dest = (char *)malloc(sizeof(char) * (i + 1));
+    i = 0;
+    while (src[i])
+    {
+       dest[i] = src[i];
+       i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+int ft_strcmp(const char *s1, const char *s2)
+{
+
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return (unsigned char)(*s1) - (unsigned char)(*s2);
+}
+/*  Counts the number of arguments in the `tokens` array starting from the `start` index
+ stopping at operators ('|', '<', '>') or the end of the array */
+int count_args(char **tokens, int start)
+{
+    int count = 0;
+    while (tokens[start] && tokens[start][0] != '|'
+        && tokens[start][0] != '<' && tokens[start][0] != '>')
+    {
+        count++;
+        start++;
+    }
+    return count;
+}
+/*Converts the `tokens` array into a linked list of `t_command` structs 
+ where each command holds arguments, input/output redirections, and possible pipe connections*/
+
+t_command *parse_tokens(char **tokens)
+{
+    int i = 0;
+    t_command *head = NULL;
+    t_command *current = NULL;
+
+    while (tokens[i])
+    {
+        t_command *node = calloc(1, sizeof(t_command));
+        int argc = count_args(tokens, i);
+        node->args = malloc(sizeof(char *) * (argc + 1));
+        int j = 0;
+        while (tokens[i] && tokens[i][0] != '<' && tokens[i][0] != '>' && tokens[i][0] != '|')
+        {
+            node->args[j++] = ft_strdup(tokens[i]);
+            i++;
+        }
+        node->args[j] = NULL;
+        node->cmd = node->args[0]; 
+        while (tokens[i] && tokens[i][0] != '|')
+        {
+            if (!ft_strcmp(tokens[i], "<"))
+                node->infile = ft_strdup(tokens[++i]);
+            else if (!ft_strcmp(tokens[i], "<<"))
+            {
+                node->infile = ft_strdup(tokens[++i]);
+                node->heredoc = 1;
+            }
+            else if (!ft_strcmp(tokens[i], ">"))
+                node->outfile = ft_strdup(tokens[++i]);
+            else if (!ft_strcmp(tokens[i], ">>"))
+            {
+                node->outfile = ft_strdup(tokens[++i]);
+                node->append = 1;
+            }
+            i++;
+        }
+        if (tokens[i] && !ft_strcmp(tokens[i], "|"))
+            i++;
+        if (!head)
+            head = node;
+        else
+            current->next = node;
+
+        current = node;
+    }
+
+    return head;
+}
+/*this just for test
+ Iterates through a linked list of `t_command` structs and prints their information, 
+including the command, its arguments, input/output redirections, and whether the input 
+ redirection is a heredoc or normal file */
+
+void print_commands(t_command *cmd)
+{
+    while (cmd)
+    {
+        printf("cmd: %s\n", cmd->cmd);
+        for (int i = 0; cmd->args[i]; i++)
+            printf("  arg[%d]: %s\n", i, cmd->args[i]);
+
+        if (cmd->infile)
+        {
+            if (cmd->heredoc)
+                printf("  infile: %s (heredoc)\n", cmd->infile);
+            else
+                printf("  infile: %s (normal)\n", cmd->infile);
+        }
+
+        if (cmd->outfile)
+        {
+            if (cmd->append)
+                printf("  outfile: %s (append)\n", cmd->outfile);
+            else
+                printf("  outfile: %s (truncate)\n", cmd->outfile);
+        }
+
+        cmd = cmd->next;
+    }
+}
+// Frees the memory allocated for the array of `tokens` and each of its elements 
+
+void free_tokens(char **tokens)
+{
+    int i = 0;
+    while (tokens[i])
+        free(tokens[i++]);
+    free(tokens);
+}
+/* Frees the memory allocated for the linked list of `t_command` structs, including 
+ their arguments, input/output redirection strings, and the command struct itself */
+
+void free_commands(t_command *cmd)
+{
+    t_command *next;
+    int i;
+
+    while (cmd)
+    {
+        next = cmd->next;
+        i = 0;
+        while (cmd->args[i])
+            free(cmd->args[i++]);
+        free(cmd->args);
+        if (cmd->infile)
+            free(cmd->infile);
+        if (cmd->outfile)
+            free(cmd->outfile);
+        free(cmd);
+
+        cmd = next;
+    }
+}
    /*                
      --> int main for TESTING input && lexing && syntax error <-- 
 	                                                              */
-
 int main(void)
 {
     char *input;
     char **tokens;
-    int i;
+    t_command *cmds;
 
     while (1)
     {
         input = get_input();
         if (!input)
-        {
-            printf("exit\n");
             break;
-        }
         if (is_only_whitespace(input))
         {
             free(input);
             continue;
         }
+
         tokens = lexer(input);
         if (!tokens)
         {
             free(input);
             continue;
         }
-        if (!check_syntax_errors(tokens))
+
+        if (check_syntax_errors(tokens))
         {
-            i = 0;
-            while (tokens[i])
-			{
-                printf("token[%d]: %s\n", i, tokens[i]);
-				i++;
-			}
-		}
-        i = 0;
-        while (tokens[i])
-            free(tokens[i++]);
-        free(tokens);
+            free_tokens(tokens);
+            free(input);
+            continue;
+        }
+
+        cmds = parse_tokens(tokens);
+        if (cmds)
+        {
+            print_commands(cmds);
+            free_commands(cmds);
+        }
+
+        free_tokens(tokens); 
         free(input);
     }
 
