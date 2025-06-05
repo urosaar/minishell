a3895ea9877a6957952dafbac5377ae32f3f1737
oussama khanfar <165305673+urosaar@users.noreply.github.com>
2025-06-05 06:13:17 +0100

    fix parser.c

--- parsing/parser.c
+++ parsing/parser.c
@@ -21,7 +21,7 @@ t_command *parse_tokens(char **tokens)
 
     while (tokens[i])
     {
-        t_command *cmd = calloc(1, sizeof(t_command));
+        t_command *cmd = ft_calloc(1, sizeof(t_command));
         if (!cmd)
             return (NULL);
 
@@ -45,42 +45,61 @@ t_command *parse_tokens(char **tokens)
         if (cmd->args[0])
             cmd->cmd = ft_strdup(cmd->args[0]);
 
+        t_redirection *last_redir = NULL;
         while (tokens[i] && tokens[i][0] != '|')
         {
+            t_redirection *redir = NULL;
             if (ft_strcmp(tokens[i], "<") == 0 && tokens[i + 1])
             {
                 i++;
-                cmd->infile = strip_quotes(tokens[i]);
+                redir = ft_calloc(1, sizeof(t_redirection));
+                redir->type = TOKEN_REDIRECT_IN;
+                redir->filename = strip_quotes(tokens[i]);
             }
             else if (ft_strcmp(tokens[i], "<<") == 0 && tokens[i + 1])
             {
                 i++;
-                cmd->infile = strip_quotes(tokens[i]);
-                cmd->heredoc = 1;
+                redir = ft_calloc(1, sizeof(t_redirection));
+                redir->type = TOKEN_HEREDOC;
+                redir->filename = strip_quotes(tokens[i]);
             }
             else if (ft_strcmp(tokens[i], ">") == 0 && tokens[i + 1])
             {
                 i++;
-                cmd->outfile = strip_quotes(tokens[i]);
-                cmd->append = 0;
+                redir = ft_calloc(1, sizeof(t_redirection));
+                redir->type = TOKEN_REDIRECT_OUT;
+                redir->filename = strip_quotes(tokens[i]);
             }
             else if (ft_strcmp(tokens[i], ">>") == 0 && tokens[i + 1])
             {
                 i++;
-                cmd->outfile = strip_quotes(tokens[i]);
-                cmd->append = 1;
+                redir = ft_calloc(1, sizeof(t_redirection));
+                redir->type = TOKEN_REDIRECT_APPEND;
+                redir->filename = strip_quotes(tokens[i]);
             }
-            i++;
+            else
+            {
+                i++;
+            }
+
+            if (redir)
+            {
+                if (!cmd->redirections)
+                    cmd->redirections = redir;
+                else
+                    last_redir->next = redir;
+                last_redir = redir;
+            }
         }
-
-        if (tokens[i] && tokens[i][0] == '|')
-            i++;
-
         if (!head)
             head = cmd;
         else
             current->next = cmd;
         current = cmd;
+
+        if (tokens[i] && tokens[i][0] == '|')
+            i++;
     }
+
     return (head);
 }
