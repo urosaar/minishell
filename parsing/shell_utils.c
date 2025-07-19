24d83a83f6ab14b6146070e1a199445f59966cf0
urosaar <oussra061@gmail.com>
2025-07-19 08:45:16 +0100

    fix export a="ls -la"

--- parsing/shell_utils.c
+++ parsing/shell_utils.c
@@ -215,39 +215,98 @@ char *expand_variables(const char *input, int last_status, t_env **env) {
     }
     return result;
 }
+
 void expand_command_vars(t_command *cmd, int last_status, t_env **env)
 {
+    char *expanded_cmd = NULL;
+    char **cmd_tokens = NULL;
+    int cmd_token_count = 0;
+
     if (cmd->cmd)
     {
-        char *tmp = expand_variables(cmd->cmd, last_status, env);
+        expanded_cmd = expand_variables(cmd->cmd, last_status, env);
         free(cmd->cmd);
-        cmd->cmd = tmp;
-    }
-    if (cmd->args)
-    {
-        int i = 0;
-        while (cmd->args[i])
+        cmd->cmd = NULL;
+        if (expanded_cmd)
         {
-            char *expanded = expand_variables(cmd->args[i], last_status, env);
-            free(cmd->args[i]);
-            cmd->args[i] = expanded;
-            i++;
+            cmd_tokens = ft_split(expanded_cmd, ' ');
+            if (cmd_tokens) {
+                while (cmd_tokens[cmd_token_count]) 
+                    cmd_token_count++;
+            }
         }
     }
-    if (cmd->infile)
+
+    char **expanded_args = NULL;
+    int arg_count = 0;
+    if (cmd->args)
     {
+        while (cmd->args[arg_count]) arg_count++;
+        expanded_args = malloc(sizeof(char*) * (arg_count + 1));
+        if (expanded_args) {
+            for (int i = 0; i < arg_count; i++) {
+                expanded_args[i] = expand_variables(cmd->args[i], last_status, env);
+            }
+            expanded_args[arg_count] = NULL;
+        }
+    }
+    if (cmd_token_count > 0)
+    {
+        int new_count = cmd_token_count + (arg_count > 0 ? arg_count - 1 : 0);
+        char **new_args = malloc(sizeof(char*) * (new_count + 1));
+        int j = 0;
+
+        for (int i = 0; i < cmd_token_count; i++) {
+            new_args[j++] = cmd_tokens[i]; 
+        }
+        if (arg_count > 0)
+        {
+            for (int i = 1; i < arg_count; i++)
+            {
+                new_args[j++] = expanded_args[i];
+            }
+            if (expanded_args[0]) free(expanded_args[0]);
+        }
+        new_args[j] = NULL;
+        cmd->cmd = ft_strdup(cmd_tokens[0]);
+        free(cmd_tokens);
+        if (cmd->args)
+        {
+            for (int i = 0; i < arg_count; i++) 
+                if (cmd->args[i]) free(cmd->args[i]);
+            free(cmd->args);
+        }
+        cmd->args = new_args;
+
+        free(expanded_args);
+    }
+    else 
+    {
+        cmd->cmd = expanded_cmd;
+        if (cmd->args && expanded_args) {
+            for (int i = 0; i < arg_count; i++) {
+                free(cmd->args[i]);
+                cmd->args[i] = expanded_args[i];
+            }
+            free(expanded_args);
+        }
+    }
+    if (cmd_token_count == 0 && expanded_cmd) {
+        free(expanded_cmd);
+    }
+    if (cmd->infile) {
         char *tmp = expand_variables(cmd->infile, last_status, env);
         free(cmd->infile);
         cmd->infile = tmp;
     }
-    if (cmd->outfile)
-    {
+
+    if (cmd->outfile) {
         char *tmp = expand_variables(cmd->outfile, last_status, env);
         free(cmd->outfile);
         cmd->outfile = tmp;
     }
-    if (cmd->heredoc_delimiter)
-    {
+
+    if (cmd->heredoc_delimiter) {
         char *tmp = expand_variables(cmd->heredoc_delimiter, last_status, env);
         free(cmd->heredoc_delimiter);
         cmd->heredoc_delimiter = tmp;
